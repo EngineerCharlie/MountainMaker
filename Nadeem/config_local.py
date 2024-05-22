aad749fcd7cf3e4c9269c80c87d9908e72800c58
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2024-05-22 18:02:29 +0200

    update gitignore and add config local for personal file paths

---
+++ Nadeem/config_local.py
@@ -0,0 +1,6 @@
+#Input your file paths  here
+
+TRAIN_DIR = "C:/.../testDataUnfiltered/mountains_256.npz"
+VAL_DIR = "C:/.../testDataUnfiltered/mountains_256.npz"
+CHECKPOINT_DISC = "C:/.../output/output5/disc.pth.tar"
+CHECKPOINT_GEN = "C:/.../output/output5/gen.pth.tar"
