ea275057d0072322a7d9fc40b52a011d5e55d73b
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2024-05-24 09:26:22 +0200

    move pytorch model to appropriate folder

---
+++ GAN/config_local.py
@@ -0,0 +1,14 @@
+# Input your file paths  here
+
+TRAIN_DIR = "testDataUnfiltered/mountains_Diego.npz"
+VAL_DIR = "testDataUnfiltered/mountains_Diego.npz"
+
+CHECKPOINT_DISC = "output/output7/disc.pth"
+CHECKPOINT_GEN = "output/output7/gen.pth"
+
+LOAD_DISC = "output/output7/disc.pth210"
+LOAD_GEN = "output/output7/gen.pth210"
+
+EVALUATION_GEN = "output/output7/gen.pth225"
+
+SAMPLE_FOLDER = "output/output7/samples/"
--- GAN/data_check.py
+++ GAN/data_check.py
@@ -1,21 +1,23 @@
-# load the prepared dataset
 from numpy import load
 from matplotlib import pyplot
+import numpy as np
 
 # load the dataset
-path = "testDataUnfiltered/"
+path = ".venv/testDataUnfiltered/"
 data = load(path + "mountains_256.npz")
 src_images, tar_images = data["arr_0"], data["arr_1"]
+in_reshaped = np.moveaxis(src_images, 3, 1)
+tar_reshaped = np.moveaxis(tar_images, 3, 1)
 print("Loaded: ", src_images.shape, tar_images.shape)
 # plot source images
 n_samples = 3
 for i in range(n_samples):
     pyplot.subplot(2, n_samples, 1 + i)
     pyplot.axis("off")
-    pyplot.imshow(src_images[i].astype("uint8"))
+    pyplot.imshow(in_reshaped[i].astype("uint8"))
 # plot target image
 for i in range(n_samples):
     pyplot.subplot(2, n_samples, 1 + n_samples + i)
     pyplot.axis("off")
-    pyplot.imshow(tar_images[i].astype("uint8"))
+    pyplot.imshow(tar_reshaped[i].astype("uint8"))
 pyplot.show()
